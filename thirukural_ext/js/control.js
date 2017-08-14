ec0e282cc34cdb954f0127675067235381c6dde8
kamalakannan <kamalkannan3592@gmail.com>
2017-08-15 02:08:23 +0530

    meanings added

--- thirukural_ext/js/control.js
+++ thirukural_ext/js/control.js
@@ -3,19 +3,22 @@ var fetchline1inLocal = function(callback) {
   $.getJSON('./assets/thirukkural.json', function(data) {
     var r = Math.round(Math.random()*(data.length-1));
 	var line1 = data[r][0];
-	var  line2= data[r][1];
-	var  author= data[0][2];
-    callback(line1, line2, author);
+	var line2= data[r][1];
+	var	tamilMeaning = data[r][2];
+	var author= data[0][3];
+    callback(line1, line2, tamilMeaning, author);
   });
 };
 
 
 var changeline1 = function() {
-  callback = function(line1, line2, author) {
+  callback = function(line1, line2, tamilMeaning, author) {
     $("p#line1, p#line2, author").fadeOut(function() {
       $("p#line1").text(line1).fadeIn(function() {
         $("p#line2").text(line2).fadeIn(function(){
-			$("p#author").text(author).fadeIn(3000);
+			$("p#author").text(author).fadeIn(function(){
+			$("p#tamilMeaning").text(tamilMeaning).fadeIn();
+			});
 		});
 		
       });
